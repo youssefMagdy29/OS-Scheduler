c0f10914302bc33b9d4b53616138bdf6e8a16a02
youssefMagdy29 <youssef.magdy29@gmail.com>
2015-03-29 17:09:10 +0200

    connecting radio buttons to toggle functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     connect(ui->spnBox_noOfPrcs, SIGNAL(valueChanged(int)), this, SLOT(numberOfProcessesChanged(int)));
+
+    connect(ui->radioBtn_FCFS, SIGNAL(toggled(bool)), this, SLOT(FCFSToggled(bool)));
+    connect(ui->radioBtn_SJF, SIGNAL(toggled(bool)), this, SLOT(SJFToggled(bool)));
+    connect(ui->radioBtn_Priority, SIGNAL(toggled(bool)), this, SLOT(priorityToggled(bool)));
+    connect(ui->radioBtn_RoundRobin, SIGNAL(toggled(bool)), this, SLOT(roundRobinToggled(bool)));
 }
 
 MainWindow::~MainWindow()
